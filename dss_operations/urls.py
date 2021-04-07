c779040b9841f8e4251cf3b811a2574486ead6f3
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-07 08:46:20 +0100

    Update to Blender endpoints

--- dss_operations/urls.py
+++ dss_operations/urls.py
@@ -1,3 +1,4 @@
+
 """flight_blender URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,19 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from flight_declaration_operations import views as flight_declaration_views
-from flight_feed_operations import views as flight_feed_views
+from django.urls import path
+from . import views as dss_views
 
-from dss_operations import views as dss_views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ping', flight_feed_views.ping),
-    path('set_air_traffic', flight_feed_views.set_air_traffic),
 
-    path('dss/', include('dss_views.urls')),    
-    path('geo_fence_ops/', include('geo_fence_operations.urls')),    
-    path('flight_declaration_ops/', include('geo_fence_operations.urls')),
-
-    
+    path('create_dss_subscription/', dss_views.create_dss_subscription),
+    path('dss_isa_callback', dss_views.dss_isa_callback),
+    path('get_rid_data/<uuid:subscription_id>', dss_views.get_rid_data),
 ]
