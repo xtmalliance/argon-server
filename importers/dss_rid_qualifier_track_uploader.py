d3aede1db12db6008ac5e92cebf17ffa74c102e2
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-05-27 16:23:05 +0100

    Updated Openskiy Imports and Track Uploader

--- importers/dss_rid_qualifier_track_uploader.py
+++ importers/dss_rid_qualifier_track_uploader.py
@@ -1,11 +1,13 @@
 import os
-from os import environ as env
+from os import environ as env, stat
 from datetime import datetime, timedelta
 from dotenv import load_dotenv, find_dotenv
 import json
+from os.path import dirname, abspath
 import requests
-import pandas as pd
 import redis
+import time
+import arrow
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -54,43 +56,35 @@ if __name__ == '__main__':
     my_credentials = PassportCredentialsGetter()
     credentials = my_credentials.get_cached_credentials()
     
-    
-    username = os.environ.get('OPENSKY_NETWORK_USERNAME')
-    password = os.environ.get('OPENSKY_NETWORK_PASSWORD')
+    parent_dir = dirname(abspath(__file__))  #<-- absolute dir the raw input file  is in
+    rel_path = "air_traffic_samples/flight_1_rid_aircraft_state.json"
+    abs_file_path = os.path.join(parent_dir, rel_path)
 
-    # bbox = (min latitude, max latitude, min longitude, max longitude)
-
-    view_port=(45.8389, 47.8229, 5.9962, 10.5226)
-    lat_min = min(view_port[0], view_port[2])
-    lat_max = max(view_port[0], view_port[2])
-    lng_min = min(view_port[1], view_port[3])
-    lng_max = max(view_port[1], view_port[3])
-
-    url_data='https://opensky-network.org/api/states/all?'+'lamin='+str(lat_min)+'&lomin='+str(lng_min)+'&lamax='+str(lat_max)+'&lomax='+str(lng_max)
-
-    response=requests.get(url_data,  auth=(username, password)).json()
-
-    #LOAD TO PANDAS DATAFRAME
-    col_name=['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground','velocity',       
-    'true_track','vertical_rate','sensors','geo_altitude','squawk','spi','position_source']
-    flight_df=pd.DataFrame(response['states'],columns=col_name)
-    flight_df=flight_df.fillna('No Data') 
-    
-    all_observations = []
-    for index, row in flight_df.iterrows():
-        metadata = {'velocity':row['velocity']}
+    with open(abs_file_path, "r") as aircraft_state_file:
+        state_json = aircraft_state_file.read()
         
-        all_observations.append({"icao_address" : row['icao24'],"traffic_source" :2, "source_type" : 1, "lat_dd" : row['lat'], "lon_dd" : row['long'], "time_stamp" :  row['time_position'],"altitude_mm" :  row['baro_altitude'], 'metadata':metadata})    
+    state_json = json.loads(state_json)
+    aircraft_type = state_json['flight_telemetry']['aircraft_type']
+
+    metadata = {"aircraft_type": aircraft_type}
+
+    flight_states = state_json['flight_telemetry']['states']
+    flight_id = state_json['flight_details']["serial_number"]
 
     headers = {"Content-Type":'application/json',"Authorization": "Bearer "+ credentials['access_token']}
     
-    payload = {"observations":all_observations}    
-    securl = 'http://localhost:8000/set_air_traffic' # set this to self (Post the json to itself)
+    for flight_state in flight_states:
+        time_stamp = arrow.now().int_timestamp
+        payload = {"observations":[{"icao_address" : flight_id,"traffic_source" :11, "source_type" : 1, "lat_dd" : flight_state['position']['lat'], "lon_dd" :  flight_state['position']['lon'], "time_stamp" : time_stamp,"altitude_mm" :  flight_state['position']['alt'], 'metadata':metadata}]}
+    
+    
+        securl = env.get("BLENDER_FQDN","http://localhost:8000/") +'/set_air_traffic' 
 
-    try:
-        response = requests.post(securl, json = payload, headers = headers)
-        
-    except Exception as e:
-        print(e)
-    else:
-        response.json()
+        time.sleep(1)
+        try:
+            response = requests.post(securl, json = payload, headers = headers)        
+        except Exception as e:
+            print(e)
+        else:
+            response.json()
+
