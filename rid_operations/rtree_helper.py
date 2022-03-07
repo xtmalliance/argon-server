c7f3db68e35d4213118c3cdf48c3dba90fc4f717
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-03-07 16:03:57 +0000

    Update rtree_helper.py

--- rid_operations/rtree_helper.py
+++ rid_operations/rtree_helper.py
@@ -6,8 +6,8 @@ class OperationalIntentsIndexFactory():
     def __init__(self, index_name:str):
         self.idx = index.Index(index_name)
 
-    def add_box_to_index(self, op_int_id, view):
-        self.idx.insert(id = op_int_id, bounds = (view[0], view[1], view[2], view[3]))
+    def add_box_to_index(self, op_int_id, view, start_time, end_time):
+        self.idx.insert(id = op_int_id, bounds = (view[0], view[1], view[2], view[3]), start_time = start_time, end_time = end_time)
 
     def generate_operational_intents_index(self) -> None:
         """This method generates a rTree index of currently active operational indexes """
@@ -23,11 +23,7 @@ class OperationalIntentsIndexFactory():
             self.add_box_to_index(op_int_id = operational_intent_id, view = view, box= view)
 
     def check_box_intersection(self, view_box):
-        
+
         intersections = [n for n in self.idx.intersection((view_box[0], view_box[1], view_box[2], view_box[3]))]        
         self.idx.close()
-
-        if intersections:
-            return True
-        else: 
-            return False
+        return intersections
