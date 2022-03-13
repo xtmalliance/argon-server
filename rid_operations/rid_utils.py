77e1c47533f9542fee0432d8a724e273bc29fde3
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-03-13 12:58:10 +0000

    First pass of test injection interface

--- rid_operations/rid_utils.py
+++ rid_operations/rid_utils.py
@@ -1,6 +1,18 @@
 from typing import List, NamedTuple,Optional
 import uuid
 from dataclasses import dataclass
+import arrow
+
+class StringBasedDateTime(str):
+  """String that only allows values which describe a datetime."""
+  def __new__(cls, value):
+    if isinstance(value, str):
+      t = arrow.get(value).datetime
+    else:
+      t = value
+    str_value = str.__new__(cls, arrow.get(t).to('UTC').format('YYYY-MM-DDTHH:mm:ss.SSSSSS') + 'Z')
+    str_value.datetime = t
+    return str_value
 
 
 class Position(NamedTuple):
@@ -9,11 +21,9 @@ class Position(NamedTuple):
     lng:float
     alt:float
 
-
 class RIDPositions(NamedTuple):
     ''' A list of positions for RID'''
     positions: List[Position]
-
 
 class RIDFlight(NamedTuple):    
     id:str
@@ -25,11 +35,9 @@ class ClusterDetails(NamedTuple):
     area_sqm: float
     number_of_flights: float
 
-
 class RIDDisplayDataResponse(NamedTuple):
     flights: List[RIDFlight]
     clusters: List[ClusterDetails]
-
 
 @dataclass
 class SubscriptionResponse():
@@ -43,3 +51,72 @@ class CreateSubscriptionResponse(NamedTuple):
     message: str
     id: uuid.uuid4
     dss_subscription_response: Optional[SubscriptionResponse]
+
+@dataclass
+class RIDAircraftPosition():
+  lat: float
+  lng: float
+  alt: float
+  accuracy_h: str
+  accuracy_v: str
+  extrapolated: Optional[bool]
+  pressure_altitude: Optional[float]
+
+@dataclass
+class RIDHeight():
+  distance: float
+  reference: str
+
+@dataclass
+class LatLngPoint():
+  lat: float
+  lng: float
+
+@dataclass
+class RIDAuthData():
+  format: str
+  data: str
+
+@dataclass
+class RIDFlightDetails():
+  id: str
+  operator_id: Optional[str]
+  operator_location: Optional[LatLngPoint]
+  operation_description: Optional[str]
+  auth_data: Optional[RIDAuthData]
+  serial_number: Optional[str]
+  registration_number: Optional[str]
+
+
+@dataclass
+class FlightState():     
+  timestamp: StringBasedDateTime
+  timestamp_accuracy: float
+  operational_status: Optional[str]
+  position: RIDAircraftPosition
+  track: float
+  speed: float
+  speed_accuracy: str
+  vertical_speed: float
+  height: Optional[RIDHeight]
+  group_radius:int
+  group_ceiling: int
+  group_floor: int
+  group_count: int
+  group_time_start: StringBasedDateTime
+  group_time_end: StringBasedDateTime
+
+@dataclass
+class RIDTestDetailsResponses: 
+    effective_after: str
+    details: RIDFlightDetails
+
+@dataclass
+class RIDTestInjection(): 
+    injection_id: uuid
+    telemetry: List[FlightState]
+    details_responses: List[RIDTestDetailsResponses]
+
+@dataclass
+class CreateTestResponse():
+    injected_flights: RIDTestInjection
