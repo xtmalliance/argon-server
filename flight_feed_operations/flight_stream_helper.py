1790ba3ab3a8da0ab420199cb04f39908e91dbd2
Hrishikesh Ballal <hballal@gmail.com>
2022-12-30 20:38:11 +0000

    removing push to spotlight for flight observations

--- flight_feed_operations/flight_stream_helper.py
+++ flight_feed_operations/flight_stream_helper.py
@@ -56,51 +56,11 @@ class ObservationReadOperations():
         
         messages = push_cg.read()
         pending_messages = []
-        
-        my_credentials = PassportCredentialsGetter()
-        credentials = my_credentials.get_cached_credentials()
-        if 'error' in credentials: 
-            logging.error('Error in getting credentials %s' % credentials)
-        else:
-            for message in messages:             
-                pending_messages.append({'timestamp': message.timestamp,'seq': message.sequence, 'msg_data':message.data, 'address':message.data['icao_address'], 'metadata':message['metadata']})
+
+        for message in messages:             
+            pending_messages.append({'timestamp': message.timestamp,'seq': message.sequence, 'msg_data':message.data, 'address':message.data['icao_address'], 'metadata':message['metadata']})
         return pending_messages
         
         
-class PassportCredentialsGetter():
-    def __init__(self):
-        pass
-
-    def get_cached_credentials(self):  
-        r = get_redis()
-        now = datetime.now()
-        cache_key = 'airtraffic_access_token_details'
-        token_details = r.get(cache_key)
-        if token_details:    
-            token_details = json.loads(token_details)
-            created_at = token_details['created_at']
-            set_date = datetime.strptime(created_at,"%Y-%m-%dT%H:%M:%S.%f")
-            if now < (set_date - timedelta(minutes=58)):
-                credentials = self.get_write_credentials()
-                r.set(cache_key, json.dumps({'credentials': credentials, 'created_at':now.isoformat()}))
-            else: 
-                credentials = token_details['credentials']
-        else:   
-            
-            credentials = self.get_write_credentials()
-            if 'error' not in credentials:
-                r.set(cache_key, json.dumps({'credentials': credentials, 'created_at':now.isoformat()}))
-                r.expire(cache_key, timedelta(minutes=58))
-        return credentials
-            
-            
-    def get_write_credentials(self):        
-        payload = {"grant_type":"client_credentials","client_id": os.getenv('SPOTLIGHT_WRITE_CLIENT_ID'),"client_secret": os.getenv('SPOTLIGHT_WRITE_CLIENT_SECRET'),"audience": os.getenv('SPOTLIGHT_AUDIENCE'),"scope": os.getenv('SPOTLIGHT_AIR_TRAFFIC_SCOPE')}        
-        url = os.getenv('PASSPORT_URL') + os.getenv('PASSPORT_TOKEN_URL')
-        token_data = requests.post(url, data = payload)
-        t_data = token_data.json()
-        
-        return t_data
-
 
 
