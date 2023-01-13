f916f25d27d87e50534a00225859892d46201c94
hrishiballal <hrishikeshballal@yahoo.com>
2023-01-13 09:58:55 +0000

    Fixed import paths

--- importers/auth_factory.py
+++ importers/auth_factory.py
@@ -1,3 +1,11 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
 import json, time, requests
 from datetime import datetime, timedelta
 from typing import List
