8f0d4fa33a0f0c0ee972929df2e2406734c0b867
Govind Singh <govind.sk85@gmail.com>
2023-02-22 15:55:16 +0400

    stop postgresql service as default service overlaps with the tcp port
    stop postgresql service  to fix below error.
    
    ERROR: for db  Cannot start service db: driver failed programming external
    connectivity on endpoint flight-blender_db_1: Error starting userland
    proxy: listen tcp4 0.0.0.0:5432: bind: address already in use
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

--- start_blender.sh
+++ start_blender.sh
@@ -2,6 +2,11 @@
 BLENDER_ROOT=.
 cp .env.sample .env
 chmod +x $BLENDER_ROOT/entrypoint.sh
+STATUS="$(systemctl is-active postgresql)"
+if [ "${STATUS}" = "active" ]; then
+    echo "stop local instance of postgresql"
+    sudo systemctl stop postgresql
+fi
 docker-compose down
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
