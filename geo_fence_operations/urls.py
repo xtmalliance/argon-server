7c7c6ae1c5040c635dd09961db771f74a901711a
Chathura Yapa <chathura.yapa@solita.fi>
2023-07-19 13:24:58 +0300

    Urls formatted

--- geo_fence_operations/urls.py
+++ geo_fence_operations/urls.py
@@ -13,20 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import views as geo_fence_views
 
 
 urlpatterns = [
-    path('set_geo_fence', geo_fence_views.set_geo_fence),
-    path('set_geozone', geo_fence_views.set_geozone),
-    path('geo_fence', geo_fence_views.GeoFenceList.as_view()),
-    path('geo_fence/<uuid:pk>', geo_fence_views.GeoFenceDetail.as_view()),
-
+    path("set_geo_fence", geo_fence_views.set_geo_fence,name="set_geo_fence"),
+    path("set_geozone", geo_fence_views.set_geozone),
+    path("geo_fence", geo_fence_views.GeoFenceList.as_view()),
+    path("geo_fence/<uuid:pk>", geo_fence_views.GeoFenceDetail.as_view()),
     # End points for automated testing interface
-    path('geo_awareness/status', geo_fence_views.GeoZoneTestHarnessStatus.as_view()),
-    path('geo_awareness/geozone_sources/<uuid:geozone_source_id>', geo_fence_views.GeoZoneSourcesOperations.as_view()),
-    path('geo_awareness/geozones/check', geo_fence_views.GeoZoneCheck.as_view()),
-
+    path("geo_awareness/status", geo_fence_views.GeoZoneTestHarnessStatus.as_view()),
+    path(
+        "geo_awareness/geozone_sources/<uuid:geozone_source_id>",
+        geo_fence_views.GeoZoneSourcesOperations.as_view(),
+    ),
+    path("geo_awareness/geozones/check", geo_fence_views.GeoZoneCheck.as_view()),
 ]
