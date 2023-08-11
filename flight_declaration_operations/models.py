52c3ebe05f309fe8082d7003dc01f83277b73314
Chathura Yapa <chathura.yapa@solita.fi>
2023-08-11 11:40:37 +0300

    Black formatted and Imports sorted

--- flight_declaration_operations/models.py
+++ flight_declaration_operations/models.py
@@ -1,37 +1,61 @@
-from django.db import models
 import uuid
 from datetime import datetime
+
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class FlightDeclaration(models.Model):
-    ''' A flight operation object for permission ''' 
-    OPERATION_TYPES = ((1, _('VLOS')),(2, _('BVLOS')),(3,_('CREWED')),)    
-    OPERATION_STATE = ((0, _('Not Submitted')),(1, _('Accepted')),(2, _('Activated')),(3,_('Nonconforming')),(4,_('Contingent')),(5,_('Ended')),)    
-    
+    """A flight operation object for permission"""
+
+    OPERATION_TYPES = (
+        (1, _("VLOS")),
+        (2, _("BVLOS")),
+        (3, _("CREWED")),
+    )
+    OPERATION_STATE = (
+        (0, _("Not Submitted")),
+        (1, _("Accepted")),
+        (2, _("Activated")),
+        (3, _("Nonconforming")),
+        (4, _("Contingent")),
+        (5, _("Ended")),
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     operational_intent = models.TextField()
     flight_declaration_raw_geojson = models.TextField(null=True, blank=True)
-    type_of_operation = models.IntegerField(choices=OPERATION_TYPES, default=1, help_text="At the moment, only VLOS and BVLOS operations are supported, for other types of operations, please issue a pull-request")
-    state = models.IntegerField(choices=OPERATION_STATE, default=0, help_text="Set the state of operation")
-    bounds = models.CharField(max_length = 140)
+    type_of_operation = models.IntegerField(
+        choices=OPERATION_TYPES,
+        default=1,
+        help_text="At the moment, only VLOS and BVLOS operations are supported, for other types of operations, please issue a pull-request",
+    )
+    state = models.IntegerField(
+        choices=OPERATION_STATE, default=0, help_text="Set the state of operation"
+    )
+    bounds = models.CharField(max_length=140)
 
-    originating_party = models.CharField(max_length=100, help_text="Set the party originating this flight, you can add details e.g. Aerobridge Flight 105", default="Flight Blender Default")
+    originating_party = models.CharField(
+        max_length=100,
+        help_text="Set the party originating this flight, you can add details e.g. Aerobridge Flight 105",
+        default="Flight Blender Default",
+    )
 
-    submitted_by = models.EmailField(blank= True, null= True)
-    approved_by = models.EmailField(blank= True, null= True)
+    submitted_by = models.EmailField(blank=True, null=True)
+    approved_by = models.EmailField(blank=True, null=True)
 
     start_datetime = models.DateTimeField(default=datetime.now)
     end_datetime = models.DateTimeField(default=datetime.now)
-    is_approved = models.BooleanField(default =False)
-    
+    is_approved = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
-        
+        ordering = ["-created_at"]
+
     def __unicode__(self):
-       return self.originating_party + ' ' + str(self.id)
+        return self.originating_party + " " + str(self.id)
 
     def __str__(self):
-        return self.originating_party + ' ' + str(self.id)
+        return self.originating_party + " " + str(self.id)
--- flight_declaration_operations/serializers.py
+++ flight_declaration_operations/serializers.py
@@ -1,40 +1,63 @@
-from rest_framework import serializers
 import json
+
+from rest_framework import serializers
+
 from .models import FlightDeclaration
 from .utils import OperationalIntentsConverter
 
+
 class FlightDeclarationSerializer(serializers.ModelSerializer):
-    operational_intent = serializers.SerializerMethodField() 
-    flight_declaration_geojson = serializers.SerializerMethodField() 
-    flight_declaration_raw_geojson = serializers.SerializerMethodField() 
-    
+    operational_intent = serializers.SerializerMethodField()
+    flight_declaration_geojson = serializers.SerializerMethodField()
+    flight_declaration_raw_geojson = serializers.SerializerMethodField()
+
     def get_flight_declaration_geojson(self, obj):
-        o = json.loads(obj.operational_intent) 
-        
+        o = json.loads(obj.operational_intent)
+
         my_operational_intent_converter = OperationalIntentsConverter()
-        my_operational_intent_converter.convert_operational_intent_to_geo_json(volumes = o['volumes'])
-        
-        return my_operational_intent_converter.geo_json   
-   
+        my_operational_intent_converter.convert_operational_intent_to_geo_json(
+            volumes=o["volumes"]
+        )
+
+        return my_operational_intent_converter.geo_json
+
     def get_flight_declaration_raw_geojson(self, obj):
-        
         return json.loads(obj.flight_declaration_raw_geojson)
 
     def get_operational_intent(self, obj):
-        return json.loads(obj.operational_intent) 
+        return json.loads(obj.operational_intent)
+
     class Meta:
         model = FlightDeclaration
-        fields = ('operational_intent','originating_party', 'type_of_operation','id','state','is_approved','start_datetime','end_datetime','flight_declaration_geojson','flight_declaration_raw_geojson','approved_by','submitted_by',)
-     
+        fields = (
+            "operational_intent",
+            "originating_party",
+            "type_of_operation",
+            "id",
+            "state",
+            "is_approved",
+            "start_datetime",
+            "end_datetime",
+            "flight_declaration_geojson",
+            "flight_declaration_raw_geojson",
+            "approved_by",
+            "submitted_by",
+        )
+
+
 class FlightDeclarationApprovalSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = FlightDeclaration
-        fields = ('is_approved','approved_by',)
-     
+        fields = (
+            "is_approved",
+            "approved_by",
+        )
+
+
 class FlightDeclarationStateSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = FlightDeclaration
-        fields = ('state','submitted_by',)
-     
+        fields = (
+            "state",
+            "submitted_by",
+        )
