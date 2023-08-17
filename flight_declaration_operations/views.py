5c5128242a561b7b67c6273048392555ea438007
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2023-08-17 16:51:18 +0100

    Update views.py

--- flight_declaration_operations/views.py
+++ flight_declaration_operations/views.py
@@ -153,8 +153,7 @@ def set_flight_declaration(request):
             meters=props["max_altitude"]["meters"], datum=props["max_altitude"]["datum"]
         )
 
-    default_state = 1  # Default state is Acccepted
-
+    default_state = 0 # Default state is Acccepted
     flight_declaration = FlightDeclarationRequest(
         features=all_features,
         type_of_operation=type_of_operation,
@@ -165,7 +164,6 @@ def set_flight_declaration(request):
     )
 
 
-    default_state = 0 # Default state is Acccepted
 
     flight_declaration = FlightDeclarationRequest(features = all_features, type_of_operation=type_of_operation, submitted_by=submitted_by, approved_by= approved_by, is_approved=is_approved, state=default_state)
 
