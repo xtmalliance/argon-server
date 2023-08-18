b366b6b63bdbd1b6b5a06b9e89520a93bd8ab33d
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2023-08-18 17:42:30 +0100

    Format update to Conformance monitoring commands

--- conformance_monitoring_operations/management/commands/check_operational_conformance.py
+++ conformance_monitoring_operations/management/commands/check_operational_conformance.py
@@ -1,21 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
 from conformance_monitoring_operations.tasks import check_flight_conformance
 
+
 class Command(BaseCommand):
     help = "Check conformance of existing operations"
 
     def add_arguments(self, parser):
-        
         parser.add_argument(
-        "-d",
-        "--dryrun",
-        dest = "dryrun",
-        metavar = "Set if this is a dry run",
-        default= '1', 
-        help='Set if it is a dry run')
-               
-    def handle(self, *args, **options):
-        
-        dry_run = options['dryrun']    
+            "-d",
+            "--dryrun",
+            dest="dryrun",
+            metavar="Set if this is a dry run",
+            default="1",
+            help="Set if it is a dry run",
+        )
 
-        check_flight_conformance.delay(dry_run = dry_run)
+    def handle(self, *args, **options):
+        dry_run = options["dryrun"]
+
+        check_flight_conformance.delay(dry_run=dry_run)
--- conformance_monitoring_operations/management/commands/update_operational_intent_to_activated.py
+++ conformance_monitoring_operations/management/commands/update_operational_intent_to_activated.py
@@ -8,117 +8,167 @@ import logging
 from auth_helper.common import get_redis
 import json
 from scd_operations.dss_scd_helper import SCDOperations
-from scd_operations.scd_data_definitions import Time, OperationalIntentReferenceDSSResponse, ImplicitSubscriptionParameters
+from scd_operations.scd_data_definitions import (
+    Time,
+    OperationalIntentReferenceDSSResponse,
+    ImplicitSubscriptionParameters,
+)
 
 
 load_dotenv(find_dotenv())
- 
+
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-logger = logging.getLogger('django')
+logger = logging.getLogger("django")
+
 
 class Command(BaseCommand):
-    
-    help = 'This command clears the operation in the DSS after the state has been set to ended.'
+    help = "This command clears the operation in the DSS after the state has been set to ended."
 
     def add_arguments(self, parser):
+        parser.add_argument(
+            "-o",
+            "--flight_declaration_id",
+            dest="flight_declaration_id",
+            metavar="ID of the flight declaration",
+            help="Specify the ID of Flight Declaration",
+        )
 
         parser.add_argument(
-        "-o",
-        "--flight_declaration_id",
-        dest = "flight_declaration_id",
-        metavar = "ID of the flight declaration",
-        help='Specify the ID of Flight Declaration')
+            "-d",
+            "--dry_run",
+            dest="dry_run",
+            metavar="Set if this is a dry run",
+            default="1",
+            help="Set if it is a dry run",
+        )
 
-        parser.add_argument(
-        "-d",
-        "--dry_run",
-        dest = "dry_run",
-        metavar = "Set if this is a dry run",
-        default= '1', 
-        help='Set if it is a dry run')
-        
-        
     def handle(self, *args, **options):
-        dry_run = options['dry_run']                 
-        dry_run = 1 if dry_run =='1' else 0
+        dry_run = options["dry_run"]
+        dry_run = 1 if dry_run == "1" else 0
 
         new_state = OPERATION_STATES[2][1]
-        
-        try:        
-            flight_declaration_id = options['flight_declaration_id']
+
+        try:
+            flight_declaration_id = options["flight_declaration_id"]
         except Exception as e:
-            raise CommandError("Incomplete command, Flight Declaration ID not provided %s"% e)
+            raise CommandError(
+                "Incomplete command, Flight Declaration ID not provided %s" % e
+            )
 
         # Get the flight declaration
 
         my_database_reader = BlenderDatabaseReader()
         now = arrow.now().isoformat()
 
-        flight_declaration = my_database_reader.get_flight_declaration_by_id(flight_declaration_id= flight_declaration_id)
-        if not flight_declaration: 
-            raise CommandError("Flight Declaration with ID {flight_declaration_id} does not exist".format(flight_declaration_id = flight_declaration_id))
+        flight_declaration = my_database_reader.get_flight_declaration_by_id(
+            flight_declaration_id=flight_declaration_id
+        )
+        if not flight_declaration:
+            raise CommandError(
+                "Flight Declaration with ID {flight_declaration_id} does not exist".format(
+                    flight_declaration_id=flight_declaration_id
+                )
+            )
 
         my_scd_dss_helper = SCDOperations()
-        flight_authorization = my_database_reader.get_flight_authorization_by_flight_declaration(flight_declaration_id=flight_declaration_id)
-    
+        flight_authorization = (
+            my_database_reader.get_flight_authorization_by_flight_declaration(
+                flight_declaration_id=flight_declaration_id
+            )
+        )
+
         operational_intent_id = flight_authorization.dss_operational_intent_id
-                
-        r = get_redis()    
-        
-        flight_opint = 'flight_opint.'  + str(flight_declaration_id)            
-        
+
+        r = get_redis()
+
+        flight_opint = "flight_opint." + str(flight_declaration_id)
+
         if r.exists(flight_opint):
             op_int_details_raw = r.get(flight_opint)
             op_int_details = json.loads(op_int_details_raw)
-            
-            reference_full = op_int_details['success_response']['operational_intent_reference']
-            dss_response_subscribers = op_int_details['success_response']['subscribers']
-            details_full = op_int_details['operational_intent_details']
+
+            reference_full = op_int_details["success_response"][
+                "operational_intent_reference"
+            ]
+            dss_response_subscribers = op_int_details["success_response"]["subscribers"]
+            details_full = op_int_details["operational_intent_details"]
             # Load existing opint details
 
-            stored_operational_intent_id= reference_full['id']
-            stored_manager = reference_full['manager']
-            stored_uss_availability = reference_full['uss_availability']
-            stored_version = reference_full['version']
-            stored_state = reference_full['state']
-            stored_ovn = reference_full['ovn']
-            stored_uss_base_url = reference_full['uss_base_url']
-            stored_subscription_id = reference_full['subscription_id']
-            
-            stored_time_start = Time(format=reference_full['time_start']['format'], value=reference_full['time_start']['value'])
-            stored_time_end = Time(format=reference_full['time_end']['format'], value=reference_full['time_end']['value'])
+            stored_operational_intent_id = reference_full["id"]
+            stored_manager = reference_full["manager"]
+            stored_uss_availability = reference_full["uss_availability"]
+            stored_version = reference_full["version"]
+            stored_state = reference_full["state"]
+            stored_ovn = reference_full["ovn"]
+            stored_uss_base_url = reference_full["uss_base_url"]
+            stored_subscription_id = reference_full["subscription_id"]
 
-            stored_volumes = details_full['volumes']
-            stored_priority = details_full['priority']
-            stored_off_nominal_volumes = details_full['off_nominal_volumes']
+            stored_time_start = Time(
+                format=reference_full["time_start"]["format"],
+                value=reference_full["time_start"]["value"],
+            )
+            stored_time_end = Time(
+                format=reference_full["time_end"]["format"],
+                value=reference_full["time_end"]["value"],
+            )
 
-            reference = OperationalIntentReferenceDSSResponse(id=stored_operational_intent_id, manager =stored_manager, uss_availability= stored_uss_availability, version= stored_version, state= stored_state, ovn = stored_ovn, time_start= stored_time_start, time_end = stored_time_end, uss_base_url=stored_uss_base_url, subscription_id=stored_subscription_id)
-                
-            if not dry_run:                 
+            stored_volumes = details_full["volumes"]
+            stored_priority = details_full["priority"]
+            stored_off_nominal_volumes = details_full["off_nominal_volumes"]
 
-                blender_base_url = env.get("BLENDER_FQDN", 0) 
+            reference = OperationalIntentReferenceDSSResponse(
+                id=stored_operational_intent_id,
+                manager=stored_manager,
+                uss_availability=stored_uss_availability,
+                version=stored_version,
+                state=stored_state,
+                ovn=stored_ovn,
+                time_start=stored_time_start,
+                time_end=stored_time_end,
+                uss_base_url=stored_uss_base_url,
+                subscription_id=stored_subscription_id,
+            )
+
+            if not dry_run:
+                blender_base_url = env.get("BLENDER_FQDN", 0)
                 for subscriber in dss_response_subscribers:
-                    subscriptions = subscriber['subscriptions']
-                    uss_base_url = subscriber['uss_base_url']
+                    subscriptions = subscriber["subscriptions"]
+                    uss_base_url = subscriber["uss_base_url"]
                     if blender_base_url == uss_base_url:
                         for s in subscriptions:
-                            subscription_id = s['subscription_id']
+                            subscription_id = s["subscription_id"]
                             break
                 # Create a new subscription to the airspace
-                
-                operational_update_response = my_scd_dss_helper.update_specified_operational_intent_reference(subscription_id= subscription_id, operational_intent_ref_id = reference.id,extents= stored_volumes, new_state= str(new_state),ovn= reference.ovn, get_airspace_keys= True )
+
+                operational_update_response = (
+                    my_scd_dss_helper.update_specified_operational_intent_reference(
+                        subscription_id=subscription_id,
+                        operational_intent_ref_id=reference.id,
+                        extents=stored_volumes,
+                        new_state=str(new_state),
+                        ovn=reference.ovn,
+                        get_airspace_keys=True,
+                    )
+                )
 
                 if operational_update_response.status == 200:
-                    logging.info("Successfully updated operational intent status for {operational_intent_id} on the DSS".format(operational_intent_id = operational_intent_id))
-                else: 
+                    logging.info(
+                        "Successfully updated operational intent status for {operational_intent_id} on the DSS".format(
+                            operational_intent_id=operational_intent_id
+                        )
+                    )
+                else:
                     logging.info("Error in updating operational intent on the DSS")
 
-            else: 
+            else:
                 logging.info("Dry run, not submitting to the DSS")
 
         else:
-            logging.info("Operational intent with {flight_declaration_id} does not exist...".format(flight_declaration_id=flight_declaration_id))
-
+            logging.info(
+                "Operational intent with {flight_declaration_id} does not exist...".format(
+                    flight_declaration_id=flight_declaration_id
+                )
+            )
