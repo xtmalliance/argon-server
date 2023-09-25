339859eb8963aae5d9e857f19f28e9770a1ad85e
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2023-09-25 12:48:42 +0100

    Added model to admin panel, added signed telemetry verificatoin

--- flight_feed_operations/admin.py
+++ flight_feed_operations/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from flight_feed_operations.models import SignedTelmetryPublicKey
+admin.site.register(SignedTelmetryPublicKey)
 # Register your models here.
--- importers/submit_signed_telemetry.py
+++ importers/submit_signed_telemetry.py
@@ -1,13 +1,11 @@
 from http_message_signatures import HTTPMessageSigner, HTTPMessageVerifier, HTTPSignatureKeyResolver, algorithms
 import requests, base64, hashlib, http_sfv
-
 from auth_factory import NoAuthCredentialsGetter
 import os
 import json
 from os.path import dirname, abspath
 import  time
 import arrow
-import requests
 from dataclasses import asdict
 import rid_definitions
 from cryptography.hazmat.backends import default_backend
@@ -15,19 +13,23 @@ from cryptography.hazmat.primitives.serialization import (
     load_pem_private_key,
     load_pem_public_key,
 )
+import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError, InvalidTokenError, InvalidKeyError, DecodeError
 # This file send signed requests to Blender and verifies responses from Blendre
 # Source: https://github.com/pyauth/http-message-signatures/blob/main/test/test.py
 
 
 class MyHTTPSignatureKeyResolver(HTTPSignatureKeyResolver):
     # This method parses and returns the public / private key based on the PEM file
-    known_pem_keys = {"test-key-rsa-pss"}
+    known_pem_keys = {"test-key-rsa-pss"}           
+    def __init__(self, jwk=None):
+        self.jwk = jwk
+    def resolve_public_key(self, key_id = None):   
+        # This Public key is not used in this script, this Public key must be converted to JWK format and uploaded to a server on the public internet. That URL must be added to Flight Blender via the Public Keys API (https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/openskies-sh/flight-blender/master/api/flight-blender-1.0.0-resolved.yaml#tag/message-signing-verification/paths/~1signing_public_key/get), Flight Blender downloads the public keys and verifies the signature.  
+        public_key = jwt.algorithms.RSAAlgorithm.from_jwk(self.jwk)
+        return public_key
+            
 
-    def resolve_public_key(self, key_id: str):
-        # This Public key is not used in this script, this Public key must be converted to JWK format and uploaded to a server on the public internet. That URL must be added to Flight Blender via the Public Keys API (https://github.com/openskies-sh/flight-blender/blob/master/api/flight-blender-1.0.0-resolved.yaml#L1118), Flight Blender downloads the public keys and verifies the signature. 
-        if key_id in self.known_pem_keys:
-            with open(f"keys/{key_id}.pem", "rb") as fh:
-                return load_pem_public_key(fh.read())
 
     def resolve_private_key(self, key_id: str):
         # Use the private key to sign requests
@@ -52,7 +54,7 @@ class BlenderUploader():
         eu_classification = rid_definitions.UAClassificationEU(category='Open', class_ = "Class0")
     
         rid_operator_details = rid_definitions.RIDOperatorDetails(
-            id="382b3308-fa11-4629-a966-84bb96d3b4db",
+            id="cbb8269e-47f5-4e76-8b2e-d38aeb0d96ed",
             uas_id = uas_id,
             operation_description="Medicine Delivery",
             operator_id='CHE-076dh0dq',
@@ -79,14 +81,26 @@ class BlenderUploader():
                 signer.sign(signed_r, created=now, label = "sig-b21", key_id="test-key-rsa-pss",covered_component_ids=(), nonce="b3k2pp5k7z-50gnwp.yemd", include_alg=False)
                 # Send the signed request
                 response = s.send(signed_r)
-
-                #TODO: Verify signed responses
-                        
+                # Use public key to verify 
+                
                 
             except Exception as e:                
-                print(e)
+                print("Error in signing message to be sent to Blender {signing_error}".format(signing_error = e))
                 break
             else:
+                #TODO: Verify signed responses
+                # Get Blender public key
+                blender_response = response.json()
+                print(blender_response)
+                blender_public_key_req = requests.get(url='http://localhost:8000/signing_public_key', headers= headers)
+                blender_public_key_jwks = blender_public_key_req.json()
+                key_resolver  = MyHTTPSignatureKeyResolver(jwk= blender_public_key_jwks['keys'][0])
+                public_key = key_resolver.resolve_public_key()
+                try: 
+                    decoded_data = jwt.decode(blender_response['signed']['signature'], key=public_key, algorithms='RS256')
+                except (ExpiredSignatureError, InvalidSignatureError, InvalidTokenError, InvalidKeyError, DecodeError) as decode_error:
+                    print("Error in verifying signed response from Blender or it is not JWT, please check Flight Blender setttings")
+
                 if response.status_code == 201:
                     print("Sleeping 3 seconds..")
                     time.sleep(3)
