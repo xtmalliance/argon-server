8237b0d904a33898fa20ba5f42e0d9d912918581
lfonosol <lance.fono@solita.fi>
2023-10-20 02:08:19 +0300

    add json support to the get_weather view. Add tests

--- weather_monitoring_operations/views.py
+++ weather_monitoring_operations/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+import json 
 
 def get_weather_data(request):
-    return HttpResponse('Hello, world. You\'re at the weather_monitoring_operations index.')
+    
+    data = {
+        "weather": "sunny",
+        "temperature": "70",
+        "wind_speed": "10",
+        "wind_direction": "N",
+        "precipitation": "0",
+        "visibility": "10",
+        "cloud_coverage": "0"
+    }
+    
+    json_data = json.dumps(data)
+    
+    return JsonResponse(json_data, safe=False)
