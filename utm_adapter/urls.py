6c6eff62a45f2b71bfc4c2cb467e033cc50163c3
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-01-04 18:28:17 +0000

    Added capabilities URl, updated api collection

--- utm_adapter/urls.py
+++ utm_adapter/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from flight_feed_operations import views as flight_feed_views
 from rid_operations import views as rid_views
 from uss_operations import views as uss_views
+from scd_operations import views as scd_views
 from flight_declaration_operations import views as flight_declaration_views
 urlpatterns = [
     # RID Operations endpoints 
@@ -13,7 +14,7 @@ urlpatterns = [
 
     # Flight Declaration Operations endpoints
     path("flight_declaration", flight_declaration_views.FlightDeclarationCreateList.as_view()),
-    
+    path("flight_declaration/capabilities", scd_views.scd_capabilities),    
     path(
         "flight_declaration_state/<uuid:pk>",
         flight_declaration_views.FlightDeclarationStateUpdate.as_view(),
