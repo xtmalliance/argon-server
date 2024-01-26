7cee309dfb0e8a221bead9715c20e134ae49f9f4
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-01-26 15:42:56 +0000

    Update celery.py

--- flight_blender/celery.py
+++ flight_blender/celery.py
@@ -2,14 +2,14 @@ import os
 
 from celery import Celery
 
-
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flight_blender.settings")
 app = Celery(
     "flight_blender",
-    include=['conformance_monitoring_operations.tasks'],
+    include=["conformance_monitoring_operations.tasks"],
     broker_connection_retry_on_startup=True,
 )
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
@@ -18,6 +18,7 @@ app.config_from_object("django.conf:settings", namespace="")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
