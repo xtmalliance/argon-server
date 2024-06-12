95b4a2d3c7251ef88a22148561f87727388dc539
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-06-12 18:54:48 +0100

    Updated sh files with db / name

--- entrypoints/no-database/entrypoint-beat.sh
+++ entrypoints/no-database/entrypoint-beat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Waiting for DBs...
-if ! wait-for-it --parallel --service redis-argon-server-server:6379; then
+if ! wait-for-it --parallel --service redis-argon-server:6379; then
     exit
 fi
 
